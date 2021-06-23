dbe73f9ec28a4cd8dca2a891b574a8184047d1ab
Leonardo Migot <leonardomigot@hotmail.com>
2021-06-23 14:16:55 -0300

    fim 23 06

--- fic_company/page filmes/main.js
+++ fic_company/page filmes/main.js
@@ -1,7 +1,40 @@
-let myText = document.getElementById("movie_name");
-myText.textContent = "Borat";
-
 document.getElementById("button").onclick = function() {
     alert("apertei");
 }
+
+document.getElementById("button2").onclick = function() {
+    alert("apertei2");
+}
+
+
+
+function createList(title, rating, year, seasons) {
+    var template = document.getElementsByTagName("template")[0]; // Elemento template
+    var clone = template.content.cloneNode(true);                // Clone do template
+
+    var test = clone.querySelectorAll("#movie_name");
+    test[0].textContent = "O Filme: " + title;   
+
+    test = clone.querySelectorAll("#wanted");
+    test[0].textContent = rating + "% Relevant";   
+
+    test = clone.querySelectorAll("#date");
+    test[0].textContent = year; 
+
+    test = clone.querySelectorAll("#season");
+    test[0].textContent = seasons + " Temporadas"; 
+
+    document.getElementById("list").appendChild(clone); 
+}
+
+
+for(var i=1; i<10; i++){
+    createList(i, ("9" + i), (2000 + i), i);
+}
+
+
+
+
+
+
 
