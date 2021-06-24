7bdf9afdb08c259b79166ed77bc2c9c82f48ec95
Leonardo Migot <leonardomigot@hotmail.com>
2021-06-24 14:14:47 -0300

    24 06 muito

--- fic_company/page filmes/main.js
+++ fic_company/page filmes/main.js
@@ -1,57 +1,111 @@
 document.getElementById("button").onclick = function() { // botao ADD
 
-    var idv = prompt("id", "id");
+    var last = localStorage.length;
     var titlev = prompt("Nome do filme");
-    var relevantv = prompt("Avaliação", "0-5/5");
+    var relevantv = prompt("Avaliação", "%");
     var datev = prompt("Data de lançamento", "Ano");
     var seasonsv = prompt("Qtd de Temporadas");
     
-    createList(idv, titlev, relevantv, datev, seasonsv);
+    localStorage.setItem(
+        last,
+        '{"title":"' + titlev + '", ' +
+        '"rating":"' + relevantv + '", ' +
+        '"year":"' + datev + '", ' +
+        '"seasons":"' + seasonsv + '"}'
+    );
+
+    loadPageItems();
 }
 
 document.getElementById("button2").onclick = function() { // botao DELETE
     var deletev = prompt("Delete", "ID");
+    //localStorage.removeItem(deletev);
+    //populateDb();
 
-    removeList(deletev);
+    for(var x=deletev; x<(localStorage.length); x++){
+
+        localStorage.setItem(x) = localStorage.getItem(x+1);
+
+        //console.log(x + " e " + parseInt(x+1));
+
+    } 
+
+    // no final é remove();
+
+    console.log(localStorage.getItem(deletev));
+
+    loadPageItems();
 }
 
-document.getElementById("click_filmes").onclick = function() {
-
+document.getElementById("click_load_all").onclick = function() {
+    loadPageItems();
 }
 
-document.getElementById("click_load_all").onclick = function() { // botao load db
+function loadPageItems() { // botao load db
+    
     clearSpace();
-    for(var i=0; i<5; i++){
-        createList(i, i, ("9" + i), (2000 + i), i); 
-    }
+
+    var numberOfItems = localStorage.length;
+
+    console.log("numero de itens " + numberOfItems);
+
+
+    for(var x=0; x < numberOfItems; x++){
+        createList(
+            x,
+            JSON.parse(localStorage[x])["title"],
+            JSON.parse(localStorage[x])["rating"],
+            JSON.parse(localStorage[x])["year"],
+            JSON.parse(localStorage[x])["seasons"]
+        );
+    }  
 }
 
 document.getElementById("click_remove_all").onclick = function() { // botao remove all
     clearSpace();
 }
 
-document.getElementById("click_visible").onclick = function() { // botao toogle visible
+document.getElementById("click_div_vis").onclick = function() {
 
-    var x = document.getElementsByClassName("entire_section");
+    var x = document.getElementById("lista1");
 
-    var size = x.length;
-
-    for(var i=0; size > i; i++){
-        console.log("TOOGLE: " + i);
-        if (x[i].style.display === "none") {
-            x[i].style.display = "block";
-        } else {
-            x[i].style.display = "none";
-        }
-    }       
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
+
+document.getElementById("click_add_vis").onclick = function() { //toogle 
+
+    var x = document.getElementById("lista2");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+function itemAdd() {
+
+    //console.log( JSON.parse(localStorage[localStorage.length-1])["title"] );
+
+
+}
+
+itemAdd();
 
 function createList(id, title, rating, year, seasons) {
     var template = document.getElementsByTagName("template")[0]; // Elemento template
     var clone = template.content.cloneNode(true);                // Clone do template
     
     var test = clone.querySelectorAll("#movie_name");    
-    test[0].textContent = "O Filme: " + title;   
+    test[0].textContent = title;   
+
+    test = clone.querySelectorAll(".identification");
+    test[0].textContent = id;
 
     test = clone.querySelectorAll("#wanted");
     test[0].textContent = rating + "% Relevant";   
@@ -65,7 +119,7 @@ function createList(id, title, rating, year, seasons) {
     test = clone.querySelectorAll(".entire_section");    
     test[0].setAttribute("id", id);
 
-    document.getElementById("list").appendChild(clone);
+    document.getElementById("lista1").appendChild(clone);
     console.log("CRIADO: " + id);
 }
 
@@ -84,4 +138,52 @@ function clearSpace() { // remover todos os itens
         elements[i].remove(); 
     }
 }
+
+function lastElementId() {    
+    var elements = document.getElementsByClassName("entire_section");    
+    var size = elements.length; 
     
+    if (size < 1) {
+        return -1;
+    } else {
+        var lastElement = elements[size-1];
+        var idLastElement = lastElement.getAttribute("id");
+        return idLastElement;
+    }    
+}
+
+function populateDb() {
+
+    localStorage.setItem(
+        "0", 
+        '{"title":"bugs", ' +
+        '"rating":"80", ' +
+        '"year":"2015", ' +
+        '"seasons":"2"}'
+    );
+
+    localStorage.setItem(
+        "1", 
+        '{"title":"second", ' +
+        '"rating":"50", ' +
+        '"year":"2008", ' +
+        '"seasons":"6"}'
+    );
+
+    localStorage.setItem(
+        "2", 
+        '{"title":"thir", ' +
+        '"rating":"5ss", ' +
+        '"year":"2002", ' +
+        '"seasons":"4"}'
+    );
+
+    //console.log(JSON.parse(localStorage[0]));
+
+    //console.log( JSON.parse(localStorage[0])["title"] );
+
+    //console.log(JSON.parse(localStorage[1]));
+}
+
+//populateDb();
+
