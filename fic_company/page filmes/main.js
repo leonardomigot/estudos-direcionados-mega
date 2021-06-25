6a3905fceceb415ff1d7c06ed942c02e75dc859f
Leonardo Migot <leonardomigot@hotmail.com>
2021-06-25 09:16:09 -0300

    25 06 - alterações com chaves localStorage

--- fic_company/page filmes/main.js
+++ fic_company/page filmes/main.js
@@ -1,3 +1,5 @@
+loadPageItems();
+
 document.getElementById("button").onclick = function() { // botao ADD
 
     var last = localStorage.length;
@@ -21,40 +23,23 @@ document.getElementById("button2").onclick = function() { // botao DELETE
     var deletev = prompt("Delete", "ID");
 
     for(var x=deletev; x<(localStorage.length-1); x++){
-
-       
-        
         var nextItem = parseInt(x) + 1;
-
-        
-        var temp = localStorage.getItem(nextItem);
-        
-        
-        localStorage.setItem(x, temp); 
-    } 
-
+        var temp = localStorage.getItem(nextItem); 
+        localStorage.setItem(x, temp);     } 
 
     localStorage.removeItem(localStorage.length-1);
 
-  
-
     loadPageItems();
 }
 
-document.getElementById("click_load_all").onclick = function() {
-    loadPageItems();
-}
-
-function loadPageItems() { // botao load db
+function loadPageItems() { 
     
     clearSpace();
 
     var numberOfItems = localStorage.length;
 
-    console.log("numero de itens " + numberOfItems);
-
-
     for(var x=0; x < numberOfItems; x++){
+
         createList(
             x,
             JSON.parse(localStorage[x])["title"],
@@ -62,11 +47,8 @@ function loadPageItems() { // botao load db
             JSON.parse(localStorage[x])["year"],
             JSON.parse(localStorage[x])["seasons"]
         );
+        
     }  
-}
-
-document.getElementById("click_remove_all").onclick = function() { // botao remove all
-    clearSpace();
 }
 
 document.getElementById("click_div_vis").onclick = function() {
@@ -90,16 +72,6 @@ document.getElementById("click_add_vis").onclick = function() { //toogle
         x.style.display = "none";
     }
 }
-
-
-function itemAdd() {
-
-    //console.log( JSON.parse(localStorage[localStorage.length-1])["title"] );
-
-
-}
-
-itemAdd();
 
 function createList(id, title, rating, year, seasons) {
     var template = document.getElementsByTagName("template")[0]; // Elemento template
@@ -143,67 +115,4 @@ function clearSpace() { // remover todos os itens
     }
 }
 
-function lastElementId() {    
-    var elements = document.getElementsByClassName("entire_section");    
-    var size = elements.length; 
-    
-    if (size < 1) {
-        return -1;
-    } else {
-        var lastElement = elements[size-1];
-        var idLastElement = lastElement.getAttribute("id");
-        return idLastElement;
-    }    
-}
-
-function populateDb() {
-
-    localStorage.setItem(
-        "0", 
-        '{"title":"zero", ' +
-        '"rating":"0", ' +
-        '"year":"0", ' +
-        '"seasons":"0"}'
-    );
-
-    localStorage.setItem(
-        "1", 
-        '{"title":"one", ' +
-        '"rating":"1", ' +
-        '"year":"1", ' +
-        '"seasons":"1"}'
-    );
-
-    localStorage.setItem(
-        "2", 
-        '{"title":"two", ' +
-        '"rating":"2", ' +
-        '"year":"2", ' +
-        '"seasons":"2"}'
-    );
-
-    localStorage.setItem(
-        "3", 
-        '{"title":"three", ' +
-        '"rating":"3", ' +
-        '"year":"3", ' +
-        '"seasons":"3"}'
-    );
-
-    localStorage.setItem(
-        "4", 
-        '{"title":"four", ' +
-        '"rating":"4", ' +
-        '"year":"4", ' +
-        '"seasons":"4"}'
-    );
-
-    //console.log(JSON.parse(localStorage[0]));
-
-    //console.log( JSON.parse(localStorage[0])["title"] );
-
-    //console.log(JSON.parse(localStorage[1]));
-}
-
-//populateDb();
 
